b9368be8908a96947c223730d0f9787987585caf
duncan.ross <duncan1874@hotmail.com>
2020-05-03 10:59:19 +1000

    membership

--- js/memberships.js
+++ js/memberships.js
@@ -1,5 +1,5 @@
 // Remove top border from first membership card
 
-var memberships = document.getElementsByClassName("membership");
+/*var memberships = document.getElementsByClassName("membership");
 var first = memberships[0];
-first.classList.add("no-top-border");
+first.classList.add("no-top-border");*?
